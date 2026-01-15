befc0df69c10f111d7bf9f5a7fae208bee4857b4
Viggo Wessén <viggo.wessen@gmail.com>
2026-01-15 13:08:12 +0100

    added graph.js, inte klar

---
+++ client/graph.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const math = window.math;
+    const Plotly = window.Plotly;
+
+    const openBtn = document.getElementById("graphOpen");
+    const win = document.getElementById("graphWindow");
+    const header = document.getElementById("graphHeader");
+
+    const input = document.getElementById("graphInput");
+    
+})
